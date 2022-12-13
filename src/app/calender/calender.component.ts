a565d2c6abfdbc53608987465def4720b0d49c8a
Sivin Saji <sivinsaji22@gmail.com>
2022-12-14 01:06:43 +0530

    Added some functionality for calender in ts file

--- src/app/calender/calender.component.ts
+++ src/app/calender/calender.component.ts
@@ -17,10 +17,21 @@ export class CalenderComponent {
   month = this.monthArray[this.date.getMonth()];
   currentDate = this.date.toDateString();
   lastDay:any = new Date(this.date.getFullYear(),this.date.getMonth()+1,0).getDate();
-
+  prevLastDay:any = new Date(this.date.getFullYear(),this.date.getMonth(),0).getDate();
+  firstDayIndex =4 ;
   public dateFunction(){
+    let count = 0;
+    console.log(this.prevLastDay);
+    
+    for(let x = this.firstDayIndex; x>0;x--){
+      this.allDates[count]=this.prevLastDay-x+1;
+      count++;
+    }
+
+
     for(let i = 1; i<=this.lastDay;i++){
-      this.allDates[i-1]=i;
+      this.allDates[count]=i;
+      count++;
     }
   }
 }
