e284d951d418698750c3b5511a45a18c4ac6f62c
sivin <sivin@strands01.local>
2022-12-15 18:24:12 +0530

    started events in calender

--- src/app/calender/calender.component.ts
+++ src/app/calender/calender.component.ts
@@ -11,13 +11,13 @@ export class CalenderComponent {
   month: string = '';
   weekDays: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   monthArray: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
   allDates: any = [];
-  events: any = [];
+  events: any = [{ date: 1670636748000, title: 'event 1' }, { date: 1670635748000, title: 'event 2' }, { date: 1670436748000, title: 'event 3' }, { date: 1620636748000, title: 'test4' }];
 
   ngOnInit(): void {
     this.dateFunction();
   }
+
   public dateFunction() {
     this.today = new Date();
     this.allDates = [];
@@ -25,37 +25,70 @@ export class CalenderComponent {
 
     const lastDay: any = new Date(this.date.getFullYear(), this.date.getMonth() + 1, 0);
     const firstDay: any = new Date(this.date.getFullYear(), this.date.getMonth(), 1);
-    const prevLastDay: any = new Date(this.date.getFullYear(), this.date.getMonth(), 0);
+    const prevMonthLastDay: any = new Date(this.date.getFullYear(), this.date.getMonth(), 0);
     const firstDayIndex = firstDay.getDay();
     const lastDayIndex = lastDay.getDay();
     const todayString: string = this.today.toDateString();
-    const calculatedDate: Date = new Date(this.date.getFullYear(), this.date.getMonth(), 30);
 
     let isFirstClass = ' first-of-month';
     for (let x = firstDayIndex; x > 0; x--) {
-      const dateVal = prevLastDay.getDate() - x + 1;
+      const dateVal = prevMonthLastDay.getDate() - x + 1;
       const calculatedDate: Date = new Date(this.date.getFullYear(), this.date.getMonth() - 1, dateVal);
-      const dateClass: string = (calculatedDate.toDateString() === todayString) ? ' current-date' : '';
-      this.allDates.push({ class: 'prev-month' + dateClass + isFirstClass, dateNumber: dateVal, date: calculatedDate });
+      const calculatedDateString: string = calculatedDate.toDateString();
+      const dateClass: string = (calculatedDateString === todayString) ? ' current-date' : '';
+      this.allDates.push({ class: 'prev-month' + dateClass + isFirstClass, dateNumber: dateVal, date: calculatedDate, dateString: calculatedDateString });
       isFirstClass = '';
     }
 
     isFirstClass = ' first-of-month';
     const lastDateOfMonth: number = lastDay.getDate();
+
     for (let i = 1; i <= lastDateOfMonth; i++) {
       const calculatedDate: Date = new Date(this.date.getFullYear(), this.date.getMonth(), i);
-      const dateClass: string = (calculatedDate.toDateString() === todayString) ? ' current-date' : '';
-      this.allDates.push({ class: 'cur-month' + dateClass + isFirstClass, dateNumber: i, date: calculatedDate });
+      const calculatedDateString: string = calculatedDate.toDateString();
+      const dateClass: string = (calculatedDateString === todayString) ? ' current-date' : '';
+      const dateOject: any = { class: 'cur-month' + dateClass + isFirstClass, dateNumber: i, date: calculatedDate, dateString: calculatedDateString };
+
+      this.allDates.push(dateOject);
       isFirstClass = '';
     }
 
     isFirstClass = ' first-of-month';
     for (let j = 1; j < (7 - lastDayIndex); j++) {
       const calculatedDate: Date = new Date(this.date.getFullYear(), this.date.getMonth() + 1, j);
-      const dateClass: string = (calculatedDate.toDateString() === todayString) ? ' current-date' : '';
-      this.allDates.push({ class: 'next-month' + dateClass + isFirstClass, dateNumber: j, date: calculatedDate });
+      const calculatedDateString: string = calculatedDate.toDateString();
+      const dateClass: string = (calculatedDateString === todayString) ? ' current-date' : '';
+      this.allDates.push({ class: 'next-month' + dateClass + isFirstClass, dateNumber: j, date: calculatedDate, dateString: calculatedDateString });
       isFirstClass = '';
     }
+
+    this.injectEventsToCalendar();
+  }
+
+  injectEventsToCalendar(): void {
+    const eventDictionary: any = {};
+    for (const event of this.events) {
+      const dateTime: Date = new Date(event.date);
+      const eventDateString: string = dateTime.toDateString();
+      eventDictionary[eventDateString] = event.title;
+    }
+
+    for (const dateObject of this.allDates) {
+      dateObject.events = eventDictionary && eventDictionary[dateObject.dateString];
+    }
+  }
+
+  randomizeEvents(): void {
+    const tempEvents: any = [];
+    const count: number = Math.round(Math.random() * 10);
+    const dateOffset: number = Math.round(Math.random() * 15);
+
+    for (let i = 0; i < count; i++) {
+      tempEvents.push({ date: new Date(this.date.getFullYear(), this.date.getMonth(), dateOffset * (i + 1)), title: 'Event ' + i });
+    }
+
+    this.events = tempEvents;
+    this.injectEventsToCalendar();
   }
 
   leftSwitch(): void {
